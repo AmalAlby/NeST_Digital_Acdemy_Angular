fdfc51b4a380cf1f4d21e0baa3be86ea8a0a5c49
AmalAlby <amalalbyn@gmail.com>
2022-11-30 17:45:03 +0530

    navigation added

--- src/app/empreg/empreg.component.ts
+++ src/app/empreg/empreg.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-empreg',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./empreg.component.css']
 })
 export class EmpregComponent {
+
+
+  constructor(private route:Router){}
+  
+  empid=""
+  firstname=""
+  lastname=""
+  houseno=""
+  housename=""
+  streetname=""
+  pincode=""
+  district=""
+  state=""
+  country=""
+  mobile=""
+  email=""
+  parentname=""
+  gender=""
+  degree=""
+  yoe=""
+  doj=""
+  username=""
+  password=""
+  cpassword=""
+
+
+  readValues=()=>{
+    if(this.password==this.cpassword){
+      alert("Registered Successfully")
+      this.route.navigate(["/employeelogin"])
+    }else{
+      alert("Password and confirm password not match")
+    }
+  }
+
 
 }
