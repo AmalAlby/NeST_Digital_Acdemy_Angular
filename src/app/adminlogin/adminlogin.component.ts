cd8537160fdb8ebf8bdde49ad30bd8f200323558
AmalAlby <amalalbyn@gmail.com>
2022-11-30 17:25:18 +0530

    navigation added

--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminlogin',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./adminlogin.component.css']
 })
 export class AdminloginComponent {
+
+  username=""
+  password=""
+
+  constructor(private route:Router){}
+
+  admlog=()=>{
+
+    let data:any={"username":this.username,"password":this.password}
+    console.log(data)
+
+    if(data.username=="admin" && data.password=="nestdigital"){
+      this.route.navigate(["/viewc"])
+    }
+
+  }
 
 }
